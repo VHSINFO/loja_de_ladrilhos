0e6b8f28bf5597a9ae11a1957423767483c535cd
VHSINFO <102195814+VHSINFO@users.noreply.github.com>
2025-08-17 14:07:09 -0300

    edição do widget
    arquivo novo

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:loja_de_ladrilhos/main1.dart';
+import 'package:loja_de_ladrilhos/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
